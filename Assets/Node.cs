cc7168f7c093514f457e74f7697c56d6bb63e1c9
Xiacari <mrobanash18022001@gmail.com>
2021-06-01 21:59:27 +0300

    project commit

---
+++ Assets/Node.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Node : MonoBehaviour
+{
+
+    public string Name;
+    public float Number;
+    public bool Processed = false;
+
+    private Text[] nameText;
+    
+    void Update()
+    {
+        
+    }
+}
